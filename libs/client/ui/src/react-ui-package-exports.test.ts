aecc6a133887295f00221bc362cb98555e088673
Noe Charmet <noe.charmet@shipfox.io>
2026-08-06 15:51:17 +0200

    Allow package export test more CI time

--- libs/client/ui/src/react-ui-package-exports.test.ts
+++ libs/client/ui/src/react-ui-package-exports.test.ts
@@ -8,31 +8,35 @@ const lineBreak = /\r?\n/;
 const packageRoot = fileURLToPath(new URL('..', import.meta.url));
 
 describe('@shipfox/react-ui published subpath exports', () => {
-  it('resolves Callout and Markdown through Node package exports', () => {
-    const resolutions = execFileSync(
-      process.execPath,
-      [
-        '--conditions=default',
-        '--input-type=module',
-        '-e',
-        [
-          "import {createRequire} from 'node:module';",
-          "const require = createRequire(process.cwd() + '/package.json');",
-          "await import('@shipfox/client-ui');",
-          "console.log(require.resolve('@shipfox/react-ui/callout'));",
-          "console.log(require.resolve('@shipfox/react-ui/markdown'));",
-        ].join('\n'),
-      ],
-      {
-        cwd: packageRoot,
-        encoding: 'utf8',
-        env: {...process.env, NODE_OPTIONS: ''},
-      },
-    );
+    it(
+      'resolves Callout and Markdown through Node package exports',
+      () => {
+        const resolutions = execFileSync(
+          process.execPath,
+          [
+            '--conditions=default',
+            '--input-type=module',
+            '-e',
+            [
+              "import {createRequire} from 'node:module';",
+              "const require = createRequire(process.cwd() + '/package.json');",
+              "await import('@shipfox/client-ui');",
+              "console.log(require.resolve('@shipfox/react-ui/callout'));",
+              "console.log(require.resolve('@shipfox/react-ui/markdown'));",
+            ].join('\n'),
+          ],
+          {
+            cwd: packageRoot,
+            encoding: 'utf8',
+            env: {...process.env, NODE_OPTIONS: ''},
+          },
+        );
 
-    expect(resolutions.trim().split(lineBreak)).toEqual([
-      expect.stringMatching(calloutRuntimePath),
-      expect.stringMatching(markdownRuntimePath),
-    ]);
-  });
+        expect(resolutions.trim().split(lineBreak)).toEqual([
+          expect.stringMatching(calloutRuntimePath),
+          expect.stringMatching(markdownRuntimePath),
+        ]);
+      },
+      15_000,
+    );
 });
