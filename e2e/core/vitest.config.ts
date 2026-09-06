2681554386977e7cfb1f860dfdf9ca6aa19b6c28
Noe Charmet <noe.charmet@shipfox.io>
2026-09-06 13:28:40 +0200

    Provide E2E core test origins

--- e2e/core/vitest.config.ts
+++ e2e/core/vitest.config.ts
@@ -1,3 +1,13 @@
 import {defineConfig, type UserConfigExport} from '@shipfox/vitest';
 
-export default defineConfig({}, import.meta.url) as UserConfigExport;
+export default defineConfig(
+  {
+    test: {
+      env: {
+        API_PUBLIC_URL: 'http://localhost:16101',
+        CLIENT_BASE_URL: 'http://localhost:5173',
+      },
+    },
+  },
+  import.meta.url,
+) as UserConfigExport;
